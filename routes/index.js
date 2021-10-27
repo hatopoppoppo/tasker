056cc3a429908c27a102b581b104358d9fcfef12
hatopoppo <yuuki_19n1101439@nnn.ed.jp>
2021-10-28 00:54:02 +0900

    ログインデータindex配下へ

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../models/user');
+
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function (req, res, next) {
+  if (req.session.usersess == undefined) {
+    res.render("login", { title: "ログイン" });
+  }
+  else {
+    const userId = req.session.usersess["user"]
+    const user = await User.findOne({
+      where: {
+        userId: userId
+      }
+    })
+    if (user) {
+      res.render('index', { title: 'Express', user: user });
+    }
+    else {
+      res.render("login", { title: "ログイン" });
+    }
+  }
 });
 
 module.exports = router;
